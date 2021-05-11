4f4df185b213ad3798c9cf366eb5719a0acdd507
Seaboard191743 <exporesource@gmail.com>
2021-05-11 18:18:52 +0300

    feat: update files

--- webpack.config.js
+++ webpack.config.js
@@ -33,15 +33,15 @@ module.exports = {
         use: ["style-loader", "css-loader", "postcss-loader"],
       },
       {
-        test: /\.png|svg|jpg|mp4|gif$/,
-        use: {
-          loader: "file-loader?name=./assets/icons/[name].[ext]",
-        },
-      },
-      {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
         use: {
           loader: "file-loader?name=./assets/fonts/[name].[ext]",
+        },
+      },
+      {
+        test: /\.png|svg|jpg|mp4|gif$/,
+        use: {
+          loader: "file-loader?name=./assets/icons/[name].[ext]",
         },
       },
     ],
